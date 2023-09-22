57bd7fab0c8a699aae16cc33d1540fcdf03fd9a3
Janice Pickron <janpickron@yahoo.com>
2023-09-22 16:06:55 -0400

    added new files

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+//  src/index.ts
+// minimal Express API
+
+import express, {Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ greeting: 'Hello World!' })
+})
+
+app.listen(4000, () => console.log('API listening on PORT ', 4000))
