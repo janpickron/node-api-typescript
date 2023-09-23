fc4b98078c026748a6aa61f25abc43f470c9097f
Janice Pickron <janpickron@yahoo.com>
2023-09-23 10:49:07 -0400

    added new folder build, update package.json

---
+++ build/index.js
@@ -0,0 +1,16 @@
+"use strict";
+//  src/index.ts
+// minimal Express API
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.json({ greeting: 'Hello World!' });
+});
+app.listen(4000, () => console.log('API listening on PORT ', 4000));
